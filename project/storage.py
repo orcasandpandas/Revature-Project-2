c963e730b2628592488a300fdc151b410084d398
Alex Dumerve <64057740+Alex2532@users.noreply.github.com>
2026-06-19 10:23:42 -0700

    storage

---
+++ project/storage.py
@@ -0,0 +1,15 @@
+import json
+
+def read_data(filepath):
+    try:
+        f = open(filepath, "r")
+        data = f.read()
+        f.close()
+        return data
+
+    except FileNotFoundError:
+        return []
+
+def write_data(filepath, data):
+    with open (filepath, "w") as write:
+        json.dump(data, write)
