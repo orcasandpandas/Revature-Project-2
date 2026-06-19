a448452d5c532758cee87cee9573974fa7c44479
orcasandpandas <149914308+orcasandpandas@users.noreply.github.com>
2026-06-19 14:15:32 -0700

    Added auth code

--- project/auth.py
+++ project/auth.py
@@ -1,2 +1,40 @@
+import secrets
+from fastapi import FastAPI, Depends, HTTPException, APIRouter
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from starlette import status
+
+router = APIRouter()
+
+security = HTTPBasic()
+
+USERS = {
+    "admin": "password123",
+    "user1": "mypassword"
+}
+
+def get_user(credentials: HTTPBasicCredentials = Depends(security)) -> str:
+    stored_password = USERS[credentials.username, ""]
+    password_correct = secrets.compare_digest(
+        credentials.password.encode('utf-8'), 
+        stored_password.encode('utf-8')
+    )
+    if not password_correct:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+@router.get("/movies/me", tags=["Auth"])
+def get_profile(username: str = Depends(get_user)):
+    return {
+        "username": username,
+        "message": f"Hello {username}, you are authenticated."
+    }
+
+@router.get("/movies", tags=["Auth"])
+def get_profile(username: str = Depends(get_user)):
+    return {"Users": [], "requested_by": username}
 
 
