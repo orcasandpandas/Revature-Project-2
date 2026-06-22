16d663fac4d288b5a627402e3105d1896f87793c
orcasandpandas <vichuang2003@gmail.com>
2026-06-22 10:26:24 -0700

    Made calls more REST compliant, added back movie search method

--- project/routers.py
+++ project/routers.py
@@ -32,7 +32,7 @@ movies_db = [{
 storage.write_data("movies.json", movies_db)
 
 # Creates movie and puts it in storage. The template is not the same as the example database
-@router.post("/Create")
+@router.post("/create")
 async def create(genre, title, year, rating):
     data = storage.read_data("movies.json")
     new_movie = {
@@ -46,20 +46,31 @@ async def create(genre, title, year, rating):
     storage.write_data("movies.json", data)
 
 # Gets movie from storage based on id
-@router.get("/ReadOne")
+@router.get("/read-one")
 async def read_one(id_number: int):
     data = storage.read_data("movies.json")
     for movie in data:
         print(type(movie))
         if movie["id"] == id_number:
             return movie
+        
+# Allows a movie to be searched by title and/or year
+@router.get("/movie-search", response_model=MovieResponse)
+def get_movie(title: str, year: Optional[int] = None):
+
+    if year is not None:
+        return {"results" : [m for m in movies_db if m["year"] == year and title.lower() in m["title"].lower()]}
+    return {"results" : [m for m in movies_db if title.lower() in m["title"].lower()]}
+
 
 # Gets list from storage
-@router.get("/ReadAll")
+@router.get("/read-all")
 async def read_all():
     data = storage.read_data("movies.json")
 
     return data
+
+
 
 # @router.patch("/Update")
 # async def update()
