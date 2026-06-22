2b232e19177689d379ab3e21ac5d74343070b9bd
aizaman-hash <aizaman@asu.edu>
2026-06-22 13:06:46 -0700

    auth for endpoints

--- project/routers.py
+++ project/routers.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
+from auth import get_user
 from models import *
 import httpx
 import storage
@@ -18,7 +19,7 @@ movies_db = storage.read_data(filepath)
 
 # Creates movie and puts it in storage. The template is not the same as the example database
 @router.post("/create", response_model=Movie)
-async def create(title, genre, year: int, rating: float):
+async def create(title, genre, year: int, rating: float, username: str = Depends(get_user)):
     genres = genre.split()
     new_movie = {
         "id": len(movies_db) + 1,
@@ -26,6 +27,7 @@ async def create(title, genre, year: int, rating: float):
         "genres": genres,
         "year": year,
         "rating": rating,
+        "created_by": username
     }
     movies_db.append(new_movie)
     storage.write_data(filepath, movies_db)
@@ -33,14 +35,14 @@ async def create(title, genre, year: int, rating: float):
 
 # Gets movie from storage based on id
 @router.get("/read-one", response_model=Movie)
-async def read_one(id_number: int):
+async def read_one(id_number: int, username: str = Depends(get_user)):
     for movie in movies_db:
         if movie["id"] == id_number:
             return movie
         
 # Allows a movie to be searched by title and/or year
 @router.get("/movie-search", response_model=MovieResponse)
-def get_movie(title: str, year: Optional[int] = None):
+def get_movie(title: str, year: Optional[int] = None, username: str = Depends(get_user)):
 
     if year is not None:
         return {"results" : [m for m in movies_db if m["year"] == year and title.lower() in m["title"].lower()]}
@@ -49,13 +51,13 @@ def get_movie(title: str, year: Optional[int] = None):
 
 # Gets list from storage
 @router.get("/read-all", response_model=MovieResponse)
-async def read_all():
+async def read_all(username: str = Depends(get_user)):
 
     return {"results": movies_db}
 
 # updates a movie detail for a movie in the list
 @router.patch("/update", response_model=Movie)
-async def update(id_number: int, key: str, new_value):
+async def update(id_number: int, key: str, new_value, username: str = Depends(get_user)):
     for movie in movies_db:
         if movie["id"] == id_number:
             if key == "year" or key == "rating":
@@ -69,7 +71,7 @@ async def update(id_number: int, key: str, new_value):
 
 # removes a value from the list
 @router.delete("/remove", response_model=Movie)
-async def update(id_number: int):
+async def update(id_number: int, username: str = Depends(get_user)):
     for movie in movies_db:
         if movie["id"] == id_number:
             movies_db.remove(movie)
